f8077af2686e4677e2c3d929bfe365db4a68d317
Baris <baris.kucuk.atilim@gmail.com>
2019-12-22 11:52:49 +0300

    secondcommit

--- pages/AramaPage.py
+++ pages/AramaPage.py
@@ -1,7 +1,6 @@
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
-
 class AramaPage(BasePage):
     secondPageLink = (By.XPATH, '//*[@id="contentListing"]/div/div/div[2]/div[4]/a[2]')
     UrunLink= (By.XPATH,'//*[@id="p-391032147"]/div[1]/a')
--- pages/UrunPage.py
+++ pages/UrunPage.py
@@ -1,5 +1,3 @@
-
-
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
