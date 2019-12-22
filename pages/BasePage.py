c7d4d6de0006bb3eea423ff88f6b50cef14eed74
Baris <baris.kucuk.atilim@gmail.com>
2019-12-22 13:30:00 +0300

    secondcommit

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 class BasePage(object):
     def __init__(self, driver):
-        driver = webdriver.chrome("C:\Users\Baris\PycharmProjects\n11TestAutomationPython\drivers\chromedriver.exe")
+
         self.driver = driver
         self.driver.implicitly_wait(5)
         self.timeout = 30
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,22 +1,19 @@
 import unittest
 from selenium import webdriver
 import pages
-from pages import MainPage
-from pages.LoginPage import LoginPage
 
-driver = webdriver.chrome("C:\Users\Baris\PycharmProjects\n11TestAutomationPython\drivers\chromedriver.exe")
-
-driver.get("https://www.n11.com/")
+driver = webdriver.chrome("C:\Users\Baris\PycharmProjects\chromedriver.exe")
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver.implicitly_wait(20)
 
     def test_something(self):
-        MainPage.click_login_button()
-        LoginPage.set_email("baris.kucuk.atilim@gmail.com")
-        LoginPage.set_password("baris201371200")
-        self.assertEqual(True, False)
+        driver.get("https://www.n11.com/")
+        pages.MainPage.click_login_button()
+        pages.LoginPage.set_email("baris.kucuk.atilim@gmail.com")
+        pages.LoginPage.set_password("baris201371200")
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
